b486f6a564b960fe71b8749f695f447061cddd42
Taylor Iske <taylori04@yahoo.com>
2022-09-12 17:27:33 -0500

    9/12 changes

--- FileEncryption.py
+++ FileEncryption.py
@@ -1,6 +1,6 @@
 codes = {'A':'!','B':'@','C':'#','D':'$','E':'%','F':'^','G':'&','H':'*','I':'(','J':')','K':'-','L':'_','M':'+','N':'=','O':'`','P':'~','Q':'{','R':'[','S':'}','T':']','U':':','V':';','W':'"','X':'<','Y':'>','Z':'0','a':'1','b':'2','c':'3','d':'4','e':'5','f':'6','g':'7','h':'8','i':'9','j':'a','k':'b','l':'c','m':'d','n':'e','o':'f','p':'g','q':'h','r':'i','s':'j','t':'k','u':'l','v':'m','w':'n','x':'o','y':'p','z':'q'}
 
-afile = open("info_security.txt", "r")
+afile = open("info_security-1.txt", "r")
 readfile = afile.read()
 afile.close()
 
@@ -13,7 +13,7 @@ for character in readfile:
         efile.write(character)
 
 efile.close()
-efile = open("info_security.txt", "r")
+efile = open("info_security-1.txt", "r")
 fileread = efile.read()
 efile.close()
 items = codes.items()
